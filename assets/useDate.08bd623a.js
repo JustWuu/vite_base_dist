06ddc8f3ab327dfdf0218627d7d123b43c24c655
ZongWen Cai <wu@acube.com.tw>
2023-05-08 20:48:10 +0800

    first commit

---
+++ assets/useDate.08bd623a.js
@@ -0,0 +1,1 @@
+class r{constructor(){}convertDate(e){const t=new Date(e);return t.getFullYear()+"/"+(t.getMonth()+1)+"/"+t.getDate()+"("+t.getHours()+":"+t.getMinutes()+":"+t.getSeconds()+")"}}export{r as C};
