a20dcd3209db3922f6cf746e55888ba69e7a67d1
ZongWen Cai <wu@acube.com.tw>
2023-05-18 11:44:25 +0800

    feat(build): v0.3.0

--- assets/useDate.08bd623a.js
+++
@@ -1,1 +0,0 @@
-class r{constructor(){}convertDate(e){const t=new Date(e);return t.getFullYear()+"/"+(t.getMonth()+1)+"/"+t.getDate()+"("+t.getHours()+":"+t.getMinutes()+":"+t.getSeconds()+")"}}export{r as C};
